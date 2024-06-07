e36a2271fc2417805e0948e75a0bfcfebe175d1c
Atxada <133334503+Atxada@users.noreply.github.com>
2024-06-07 13:55:07 +0700

    Add files via upload
    Additional files

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Aldo Aldrich",
+    author_email='atxadaaldo17022@gmail.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+    entry_points={
+        'console_scripts': [
+            'template=template.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='node_editor',
+    name='node_editor',
+    #packages=find_packages(include=['template', 'template.*']),
+    packages=find_packages(include=['GUI*'], exclude=['ZCore*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/Atxada/template',
+    version='0.1.0',
+    zip_safe=False,
+)
