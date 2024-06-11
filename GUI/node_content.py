c1113cfb545b8a7ed73d5ce94b42cd3a6233902d
Atxada <133334503+Atxada@users.noreply.github.com>
2024-06-11 14:45:51 +0700

    Update node_content.py

--- GUI/node_content.py
+++ GUI/node_content.py
@@ -2,7 +2,7 @@
 """This module containing base class for Node's content graphical representation. It also contains example of overriden
 Text Widget which can pass to it's parent notification about currently being modified."""
 
-class NodeContentWidget(QtWidgets.QWidget, Serializable):
+class NodeContentWidget():
     """Base class: Node's graphics content. This class also provides layout for other widgets inside of :py:class:`~GUI.node_creator.NodeConfig` class"""
     def __init__(self, node, parent=None): # colon used to specify inside docs what type to pass ("string if u don't want to import/define")
         """
