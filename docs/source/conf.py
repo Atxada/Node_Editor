b2027a4465da4d2f896402be084a19d6677e229b
Atxada <133334503+Atxada@users.noreply.github.com>
2024-06-11 15:05:43 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,3 +66,42 @@ pygments_style = None
 html_theme = 'sphinx_rtd_theme'    # [classic / alabaster]
 html_theme_path = ["_themes", ]
 html_static_path = ['_static']
+
+htmlhelp_basename = 'NodeEditordoc'
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
+
+latex_documents = [
+    (master_doc, 'NodeEditor.tex', 'NodeEditor Documentation',
+     'Pavel Křupala', 'manual'),
+]
+
+man_pages = [
+    (master_doc, 'nodeeditor', 'NodeEditor Documentation',
+     [author], 1)
+]
+
+texinfo_documents = [
+    (master_doc, 'NodeEditor', 'NodeEditor Documentation',
+     author, 'NodeEditor', 'One line description of project.',
+     'Miscellaneous'),
+]
+
+epub_title = project
+
+epub_exclude_files = ['search.html']
