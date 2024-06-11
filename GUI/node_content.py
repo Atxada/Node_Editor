62d0aedfcad8a00e0372e6f775ce0b9915e5d42a
Atxada <133334503+Atxada@users.noreply.github.com>
2024-06-11 14:47:13 +0700

    Update node_content.py

--- GUI/node_content.py
+++ GUI/node_content.py
@@ -2,9 +2,9 @@
 """This module containing base class for Node's content graphical representation. It also contains example of overriden
 Text Widget which can pass to it's parent notification about currently being modified."""
 
-class NodeContentWidget():
+class NodeContentWidget:
     """Base class: Node's graphics content. This class also provides layout for other widgets inside of :py:class:`~GUI.node_creator.NodeConfig` class"""
-    def __init__(self, node, parent=None): # colon used to specify inside docs what type to pass ("string if u don't want to import/define")
+    def __init__(self): # colon used to specify inside docs what type to pass ("string if u don't want to import/define")
         """
 
         :param node: reference to :py:class:`~GUI.node_creator.NodeConfig`
@@ -12,30 +12,3 @@ class NodeContentWidget():
         :param parent: parent widget
         :type parent: QtWidgets.QWidget
         """
-
-    def initUI(self):
-        """Default layout setup and widgets to be rendered in :py:class:`~GUI.node_creator.NodeGraphics` class"""
-
-    def addWidgetToLayout(self, widgets=[], height=20):
-        """Let content create default layout configuration"""
-
-    def setEditingFlag(self, value):
-        """Helper function which sets editingFlag inside :py:class:`~GUI.node_editor.GraphicsView` class
-
-        This is a helper function to handle editing node's content with ``QLineEdits`` or ``QTextEdits`` (use overriden :py:classs:TextEditOverride)
-        and handle ``keyPressEvent`` inside :py:class:`~GUI.node_editor.GraphicsView` class.
-
-        .. note::
-
-            If you are handling KeyPress events by default Qt Window's shortcuts and ``QActions``, you can ignore this method
-
-        :param value: new value for editing flag
-        :type value: ``bool``
-        """
-
-    def serialize(self):
-        return OrderedDict([    
-        ])
-    
-    def deserialize(self, data, hashmap={}, restore_id=True):
-        return True
