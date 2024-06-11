5283249b47e1a0e47c8b68084c1b8541d9f7982e
Atxada <133334503+Atxada@users.noreply.github.com>
2024-06-12 05:37:49 +0700

    Update node_content.py

--- GUI/node_content.py
+++ GUI/node_content.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """This module containing base class for Node's content graphical representation. It also contains example of overriden
-Text Widget which can pass to it's parent notification about currently being modified. TESTING"""
+Text Widget which can pass to it's parent notification about currently being modified."""
 
 class NodeContentWidget(QtWidgets.QWidget, Serializable):
     """Base class: Node's graphics content. This class also provides layout for other widgets inside of :py:class:`~GUI.node_creator.NodeConfig` class"""
